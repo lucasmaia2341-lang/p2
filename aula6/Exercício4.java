32eb52334112b6c5e325123a87d44ef7d414cde8
teak <lucasmaia2341@gmail.com>
2026-04-08 11:21:26 -0300

    Exercício4.java

---
+++ aula6/Exercício4.java
@@ -0,0 +1,12 @@
+public class exercicio4 {
+  public static void main (String[] args) {
+ 
+    String texto = "Progamação";
+
+    String metade1 = texto.substring(0, 7);
+    String metade2 = texto.substring(6);
+
+    System.out.println("A primeira parte é: " + metade1);
+    System.out.println("A segunda parte é: " + metade2);
+  }
+}
