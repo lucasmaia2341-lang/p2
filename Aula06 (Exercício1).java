7676ccfba8210916fc7139f59352dae50aec2426
teak <lucasmaia2341@gmail.com>
2026-04-08 10:59:12 -0300

    Create Aula06 (Exercício1).java

---
+++ Aula06 (Exercício1).java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class aulaa7 {
+  public static void main (String[] args) {
+    Scanner SC = new Scanner (System.in);
+
+    System.out.print("Digite o primeiro nome: ");
+    String name1 = SC.nextLine();
+
+    System.out.println("Digite o segundo nome: ");
+    String name2 = SC.nextLine();
+
+    if (name1.equals(name2)) {
+      System.out.println("OS nomes são iguais.");
+    } else {
+      System.out.println("Os nomes são diferentes");
+    }
+
+    SC.close();
+  }
+}  
+    
