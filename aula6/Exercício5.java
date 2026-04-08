410439a109107871dd1a2dfebc94e8cbb588e540
teak <lucasmaia2341@gmail.com>
2026-04-08 11:28:53 -0300

    Exercício5.java

---
+++ aula6/Exercício5.java
@@ -0,0 +1,11 @@
+public class exercicio5 {
+  public static void main (String[] args) {
+ 
+    String texto = "computação";
+
+    System.out.println("O primeiro caractere é: " + texto.charAt(0));
+    System.out.println("O terceiro caractere é: " + texto.charAt(2));
+    System.out.println("O ultimo caractere é: " + texto.charAt(9));
+  }
+}
+    
