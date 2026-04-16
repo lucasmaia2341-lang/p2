83a32504d869a096f0d8127b134502ddfbf87335
teak <lucasmaia2341@gmail.com>
2026-04-15 21:45:08 -0300

    Create Exercício 03.java

---
+++ Aula 03/Exercício 03.java
@@ -0,0 +1,23 @@
+public class exercico3 {
+
+    public static void main(String[] args) {
+
+        double num1 = 65;
+        double num2 = 5;
+        double resultado;
+
+        resultado = num1 + num2;
+        System.out.println(resultado);
+
+        resultado = num1 - num2;
+        System.out.println(resultado);
+
+        resultado = num1 * num2;
+        System.out.println(resultado);
+
+        resultado = num1 / num2;
+        System.out.println(resultado);
+
+    }
+
+}
