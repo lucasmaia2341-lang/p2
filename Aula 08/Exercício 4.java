07cb28f3ce4cee70191dc4bef2fe2b63642c1e82
teak <lucasmaia2341@gmail.com>
2026-04-15 22:07:14 -0300

    Create Exercício 4.java

---
+++ Aula 08/Exercício 4.java
@@ -0,0 +1,8 @@
+public class Exercicio4 {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Lançar!");
+    }
+}
