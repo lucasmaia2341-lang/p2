507ecd7241ca01143d4a7d90415a59d6a08f693a
teak <lucasmaia2341@gmail.com>
2026-04-15 22:08:31 -0300

    Create Exercício 7.java

---
+++ Aula 08/Exercício 7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exercicio7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite uma palavra: ");
+        String palavra = scanner.nextLine();
+        String invertida = "";
+
+        for (int i = palavra.length() - 1; i >= 0; i--) {
+            invertida += palavra.charAt(i);
+        }
+
+        System.out.println("Invertida: " + invertida);
+        scanner.close();
+    }
+}
