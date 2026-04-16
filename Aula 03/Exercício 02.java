96ceeadcd37ae16100928451fcd7c1d405a6c041
teak <lucasmaia2341@gmail.com>
2026-04-15 21:44:36 -0300

    Create Exercício 02.java

---
+++ Aula 03/Exercício 02.java
@@ -0,0 +1,14 @@
+public class exercicio2 {
+
+    public static void main(String[] args) {
+        
+        String nome = "Lucas";
+        int idade = 22;
+        double altura = 1.71;
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(altura);
+    }
+    
+}
