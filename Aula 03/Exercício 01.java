da7e03931e8fe34bffdc0d87bc23c635553f770b
teak <lucasmaia2341@gmail.com>
2026-04-15 21:45:41 -0300

    Create Exercício 01.java

---
+++ Aula 03/Exercício 01.java
@@ -0,0 +1,12 @@
+public class exercicio1 {
+
+    public static void main(String[] args) {
+        
+        String name = "Lucas" ;
+        String sobrenome = "Maia" ;
+
+        System.out.println(name);
+        System.out.println(sobrenome);
+    }
+    
+}
