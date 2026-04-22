12dcafe3622964fb465367714790b49c0bc28b1c
teak <lucasmaia2341@gmail.com>
2026-04-22 10:50:29 -0300

    Create Exercício 1.java

---
+++ Aula 09/Exercício 1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static double calcularHipotenusa(double a, double b) {
+        return Math.sqrt(a * a + b * b);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o valor do cateto a: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Digite o valor do cateto b: ");
+        double b = scanner.nextDouble();
+
+        double resultado = calcularHipotenusa(a, b);
+
+        System.out.println("A hipotenusa é: " + resultado);
+
+        scanner.close();
+    }
+}
